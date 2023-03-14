ec637dd39a62d7bc7115073616a3093926716551
2019108102540374 <2019108102540374@lab01-12.ifgoiano.local>
2023-03-14 09:50:19 -0300

    Commit inicial do projeto

---
+++ cadastro-web/src/main/java/br/edu/ifgoiano/entidade/Usuario.java
@@ -0,0 +1,48 @@
+package br.edu.ifgoiano.entidade;
+
+import java.util.Date;
+
+public class Usuario {
+
+	private String nome; 
+	private String senha;
+	private String email;
+	private Date dataDeNascimento;
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public Date getDataDeNascimento() {
+		return dataDeNascimento;
+	}
+
+	public void setDataDeNascimento(Date dataDeNascimento) {
+		this.dataDeNascimento = dataDeNascimento;
+	}
+	
+	public Integer obterIdade() {
+		return 0;
+	}
+	
+}
